d3053909e9ce8799f31cdaa3253fd63825739b45
XiaoBeing <94702632+Mr-jiangrj@users.noreply.github.com>
2023-04-06 16:59:32 +0800

    Update music_build.sh

--- music_build.sh
+++ music_build.sh
@@ -10,6 +10,8 @@ grep "茂茂" -rl ./ | xargs sed -i "s#茂茂#@小江不emo#g"
 grep "https://github.com/maomao1996" -rl ./ | xargs sed -i "s#https://github.com/maomao1996#https://github.com/Mr-jiangrj#g"
 grep "\"author\": \"maomao1996 <1714487678@qq.com>\"" -rl ./ | xargs sed -i "s#\"author\": \"maomao1996 <1714487678@qq.com>\"#\"author\": \"@小江不emo <1326890495@qq.com>\"#g"
 grep "mmPlayer在线音乐播放器" -rl ./ | xargs sed -i "s#mmPlayer在线音乐播放器#Radios在线音乐播放器#g"
+grep "死鬼去哪里了！" -rl ./ | xargs sed -i "s#死鬼去哪里了！#客官这么快就要走了嘛~#g"
+grep "(つェ⊂)咦!又好了!" -rl ./ | xargs sed -i "s#(つェ⊂)咦!又好了!#客官来点音乐嘛~#g"
 
 npm run build
 
