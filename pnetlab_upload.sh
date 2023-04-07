dfbb4eaf36fc5dfef9d542431ce8d4b54dddba72
XiaoBeing <94702632+Mr-jiangrj@users.noreply.github.com>
2023-04-08 02:15:28 +0800

    Update pnetlab_upload.sh

--- pnetlab_upload.sh
+++ pnetlab_upload.sh
@@ -1,19 +1,18 @@
 mkdir -p /opt/pnetlab/
 cd /opt/pnetlab/
 install_pnetlab_v6=https://unetlab.cloud/api/raw/?path=/UNETLAB%20I/upgrades_pnetlab/Focal/install_pnetlab_v6.sh
-wget --content-disposition -q --no-check-certificate $install_pnetlab_v6
-sed -i 's#--show-progress    #--show-progress --no-check-certificate #g' install_pnetlab_v6.sh
+wget --content-disposition -q --show-progress $install_pnetlab_v6
 cat install_pnetlab_v6.sh | grep -E "^URL_|^wget " > download_pnetlab_v6.sh
-sed -i 's#https://unetlab.cloud/api/raw/?path=/UNETLAB%20I/upgrades_pnetlab/Focal/L/linux-5.17.15-pnetlab-uksm/#https://pan.xiaobeing.cn/api/raw/?path=/pnetlab/#g' install_pnetlab_v6.sh
-sed -i 's#https://unetlab.cloud/api/raw/?path=/UNETLAB%20I/upgrades_pnetlab/Focal/D/#https://pan.xiaobeing.cn/api/raw/?path=/pnetlab/#g' install_pnetlab_v6.sh
-sed -i 's#https://unetlab.cloud/api/raw/?path=/UNETLAB%20I/upgrades_pnetlab/Focal/P/PNET_GUACAMOLE/#https://pan.xiaobeing.cn/api/raw/?path=/pnetlab/#g' install_pnetlab_v6.sh
-sed -i 's#https://unetlab.cloud/api/raw/?path=/UNETLAB%20I/upgrades_pnetlab/Focal/P/PNET_DYNAMIPS/#https://pan.xiaobeing.cn/api/raw/?path=/pnetlab/#g' install_pnetlab_v6.sh
-sed -i 's#https://unetlab.cloud/api/raw/?path=/UNETLAB%20I/upgrades_pnetlab/Focal/P/PNET_SCHEMA/#https://pan.xiaobeing.cn/api/raw/?path=/pnetlab/#g' install_pnetlab_v6.sh
-sed -i 's#https://unetlab.cloud/api/raw/?path=/UNETLAB%20I/upgrades_pnetlab/Focal/P/PNET_VPC/#https://pan.xiaobeing.cn/api/raw/?path=/pnetlab/#g' install_pnetlab_v6.sh
-sed -i 's#https://unetlab.cloud/api/raw/?path=/UNETLAB%20I/upgrades_pnetlab/Focal/P/PNET_QEMU/#https://pan.xiaobeing.cn/api/raw/?path=/pnetlab/#g' install_pnetlab_v6.sh
-sed -i 's#https://unetlab.cloud/api/raw/?path=/UNETLAB%20I/upgrades_pnetlab/Focal/P/PNET_DOCKER/#https://pan.xiaobeing.cn/api/raw/?path=/pnetlab/#g' install_pnetlab_v6.sh
-sed -i 's#https://unetlab.cloud/api/raw/?path=/UNETLAB%20I/upgrades_pnetlab/Focal/P/PNET_PNETLAB/#https://pan.xiaobeing.cn/api/raw/?path=/pnetlab/#g' install_pnetlab_v6.sh
-sed -i 's#https://unetlab.cloud/api/raw/?path=/UNETLAB%20I/upgrades_pnetlab/Focal/P/PNET_WIRESHARK/#https://pan.xiaobeing.cn/api/raw/?path=/pnetlab/#g' install_pnetlab_v6.sh
+sed -i 's#https://unetlab.cloud/api/raw/?path=/UNETLAB%20I/upgrades_pnetlab/Focal/L/linux-5.17.15-pnetlab-uksm/#https://qiniu.xiaobeing.cn/#g' install_pnetlab_v6.sh
+sed -i 's#https://unetlab.cloud/api/raw/?path=/UNETLAB%20I/upgrades_pnetlab/Focal/D/#https://qiniu.xiaobeing.cn/#g' install_pnetlab_v6.sh
+sed -i 's#https://unetlab.cloud/api/raw/?path=/UNETLAB%20I/upgrades_pnetlab/Focal/P/PNET_GUACAMOLE/#https://qiniu.xiaobeing.cn/#g' install_pnetlab_v6.sh
+sed -i 's#https://unetlab.cloud/api/raw/?path=/UNETLAB%20I/upgrades_pnetlab/Focal/P/PNET_DYNAMIPS/#https://qiniu.xiaobeing.cn/#g' install_pnetlab_v6.sh
+sed -i 's#https://unetlab.cloud/api/raw/?path=/UNETLAB%20I/upgrades_pnetlab/Focal/P/PNET_SCHEMA/#https://qiniu.xiaobeing.cn/#g' install_pnetlab_v6.sh
+sed -i 's#https://unetlab.cloud/api/raw/?path=/UNETLAB%20I/upgrades_pnetlab/Focal/P/PNET_VPC/#https://qiniu.xiaobeing.cn/#g' install_pnetlab_v6.sh
+sed -i 's#https://unetlab.cloud/api/raw/?path=/UNETLAB%20I/upgrades_pnetlab/Focal/P/PNET_QEMU/#https://qiniu.xiaobeing.cn/#g' install_pnetlab_v6.sh
+sed -i 's#https://unetlab.cloud/api/raw/?path=/UNETLAB%20I/upgrades_pnetlab/Focal/P/PNET_DOCKER/#https://qiniu.xiaobeing.cn/#g' install_pnetlab_v6.sh
+sed -i 's#https://unetlab.cloud/api/raw/?path=/UNETLAB%20I/upgrades_pnetlab/Focal/P/PNET_PNETLAB/#https://qiniu.xiaobeing.cn/#g' install_pnetlab_v6.sh
+sed -i 's#https://unetlab.cloud/api/raw/?path=/UNETLAB%20I/upgrades_pnetlab/Focal/P/PNET_WIRESHARK/#https://qiniu.xiaobeing.cn/#g' install_pnetlab_v6.sh
 echo ""
 echo "--------------------  PNETLAB File Download Shell --------------------"
 echo ""
@@ -25,12 +24,20 @@ echo "--------------------  PNETLAB New Install Shell --------------------"
 echo ""
 cat install_pnetlab_v6.sh | grep -E "^URL_|^wget "
 echo ""
-echo "--------------------  Upload to Github Repo --------------------"
+echo "--------------------  Upload to Cloud --------------------"
 cd /root/
 wget https://github.com/MoeClub/OneList/raw/master/OneDriveUploader/amd64/linux/OneDriveUploader
 chmod 777 OneDriveUploader
-cd /opt/
+cd /opt/pnetlab
 wget https://github.com/Mr-jiangrj/website_build/raw/main/onedrive.zip
 unzip onedrive.zip
 chmod 777 auth.json
-/root/OneDriveUploader -t 18 -s "pnetlab" -r "Share"
+/root/OneDriveUploader -t 18 -s "install_pnetlab_v6.sh" -r "Share"
+cd /root/
+wget https://devtools.qiniu.com/qshell-v2.10.0-linux-amd64.tar.gz
+tar xf qshell-v2.10.0-linux-amd64.tar.gz
+mv qshell /usr/bin/
+wget https://github.com/Mr-jiangrj/website_build/raw/main/qiniu.zip
+unzip qiniu.zip
+bash qiniu.sh
+qshell qupload qupload.conf
