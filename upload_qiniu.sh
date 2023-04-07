da1f3cf8cd33d3d8f47292f571ca658bb249699b
XiaoBeing <94702632+Mr-jiangrj@users.noreply.github.com>
2023-04-08 05:47:59 +0800

    Create upload_qiniu.sh

---
+++ upload_qiniu.sh
@@ -0,0 +1,9 @@
+wget https://devtools.qiniu.com/qshell-v2.10.0-linux-amd64.tar.gz
+tar xf qshell-v2.10.0-linux-amd64.tar.gz
+mv qshell /usr/bin/
+wget https://github.com/Mr-jiangrj/website_build/raw/main/qiniu.zip
+unzip qiniu.zip
+# bash qiniu.sh
+# for list in $(ls /opt/pnetlab/);do qshell delete xiaobeing $list;done
+# qshell qupload qupload.conf
+# cat /opt/pnetlab/install_pnetlab_v6.sh | grep "^URL_"
