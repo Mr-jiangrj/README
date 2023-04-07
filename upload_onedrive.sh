47f9483d0a2be51e0e92c5f0266b7cb61f2f2363
XiaoBeing <94702632+Mr-jiangrj@users.noreply.github.com>
2023-04-08 05:32:01 +0800

    Update upload_onedrive.sh

--- upload_onedrive.sh
+++ upload_onedrive.sh
@@ -4,4 +4,4 @@ mv OneDriveUploader /usr/bin/
 wget https://github.com/Mr-jiangrj/website_build/raw/main/onedrive.zip
 unzip onedrive.zip
 rm -rf onedrive.zip
-OneDriveUploader -t 18 -s "*" -r "Share"
+OneDriveUploader -t 18 -s "./" -r "Share"
