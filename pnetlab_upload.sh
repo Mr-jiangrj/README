cae7ce78d7ca2ef2d34a22b7afcc717dfa1ff7fd
XiaoBeing <94702632+Mr-jiangrj@users.noreply.github.com>
2023-04-08 02:55:36 +0800

    Update pnetlab_upload.sh

--- pnetlab_upload.sh
+++ pnetlab_upload.sh
@@ -3,16 +3,16 @@ cd /opt/pnetlab/
 install_pnetlab_v6=https://unetlab.cloud/api/raw/?path=/UNETLAB%20I/upgrades_pnetlab/Focal/install_pnetlab_v6.sh
 wget --content-disposition -q --show-progress $install_pnetlab_v6
 cat install_pnetlab_v6.sh | grep -E "^URL_|^wget " > download_pnetlab_v6.sh
-sed -i 's#https://unetlab.cloud/api/raw/?path=/UNETLAB%20I/upgrades_pnetlab/Focal/L/linux-5.17.15-pnetlab-uksm/#https://qiniu.xiaobeing.cn/#g' install_pnetlab_v6.sh
-sed -i 's#https://unetlab.cloud/api/raw/?path=/UNETLAB%20I/upgrades_pnetlab/Focal/D/#https://qiniu.xiaobeing.cn/#g' install_pnetlab_v6.sh
-sed -i 's#https://unetlab.cloud/api/raw/?path=/UNETLAB%20I/upgrades_pnetlab/Focal/P/PNET_GUACAMOLE/#https://qiniu.xiaobeing.cn/#g' install_pnetlab_v6.sh
-sed -i 's#https://unetlab.cloud/api/raw/?path=/UNETLAB%20I/upgrades_pnetlab/Focal/P/PNET_DYNAMIPS/#https://qiniu.xiaobeing.cn/#g' install_pnetlab_v6.sh
-sed -i 's#https://unetlab.cloud/api/raw/?path=/UNETLAB%20I/upgrades_pnetlab/Focal/P/PNET_SCHEMA/#https://qiniu.xiaobeing.cn/#g' install_pnetlab_v6.sh
-sed -i 's#https://unetlab.cloud/api/raw/?path=/UNETLAB%20I/upgrades_pnetlab/Focal/P/PNET_VPC/#https://qiniu.xiaobeing.cn/#g' install_pnetlab_v6.sh
-sed -i 's#https://unetlab.cloud/api/raw/?path=/UNETLAB%20I/upgrades_pnetlab/Focal/P/PNET_QEMU/#https://qiniu.xiaobeing.cn/#g' install_pnetlab_v6.sh
-sed -i 's#https://unetlab.cloud/api/raw/?path=/UNETLAB%20I/upgrades_pnetlab/Focal/P/PNET_DOCKER/#https://qiniu.xiaobeing.cn/#g' install_pnetlab_v6.sh
-sed -i 's#https://unetlab.cloud/api/raw/?path=/UNETLAB%20I/upgrades_pnetlab/Focal/P/PNET_PNETLAB/#https://qiniu.xiaobeing.cn/#g' install_pnetlab_v6.sh
-sed -i 's#https://unetlab.cloud/api/raw/?path=/UNETLAB%20I/upgrades_pnetlab/Focal/P/PNET_WIRESHARK/#https://qiniu.xiaobeing.cn/#g' install_pnetlab_v6.sh
+sed -i 's#https://labhub.eu.org/api/raw/?path=/UNETLAB%20I/upgrades_pnetlab/Focal/L/linux-5.17.15-pnetlab-uksm/#https://qiniu.xiaobeing.cn/#g' install_pnetlab_v6.sh
+sed -i 's#https://labhub.eu.org/api/raw/?path=/UNETLAB%20I/upgrades_pnetlab/Focal/D/#https://qiniu.xiaobeing.cn/#g' install_pnetlab_v6.sh
+sed -i 's#https://labhub.eu.org/api/raw/?path=/UNETLAB%20I/upgrades_pnetlab/Focal/P/PNET_GUACAMOLE/#https://qiniu.xiaobeing.cn/#g' install_pnetlab_v6.sh
+sed -i 's#https://labhub.eu.org/api/raw/?path=/UNETLAB%20I/upgrades_pnetlab/Focal/P/PNET_DYNAMIPS/#https://qiniu.xiaobeing.cn/#g' install_pnetlab_v6.sh
+sed -i 's#https://labhub.eu.org/api/raw/?path=/UNETLAB%20I/upgrades_pnetlab/Focal/P/PNET_SCHEMA/#https://qiniu.xiaobeing.cn/#g' install_pnetlab_v6.sh
+sed -i 's#https://labhub.eu.org/api/raw/?path=/UNETLAB%20I/upgrades_pnetlab/Focal/P/PNET_VPC/#https://qiniu.xiaobeing.cn/#g' install_pnetlab_v6.sh
+sed -i 's#https://labhub.eu.org/api/raw/?path=/UNETLAB%20I/upgrades_pnetlab/Focal/P/PNET_QEMU/#https://qiniu.xiaobeing.cn/#g' install_pnetlab_v6.sh
+sed -i 's#https://labhub.eu.org/api/raw/?path=/UNETLAB%20I/upgrades_pnetlab/Focal/P/PNET_DOCKER/#https://qiniu.xiaobeing.cn/#g' install_pnetlab_v6.sh
+sed -i 's#https://labhub.eu.org/api/raw/?path=/UNETLAB%20I/upgrades_pnetlab/Focal/P/PNET_PNETLAB/#https://qiniu.xiaobeing.cn/#g' install_pnetlab_v6.sh
+sed -i 's#https://labhub.eu.org/api/raw/?path=/UNETLAB%20I/upgrades_pnetlab/Focal/P/PNET_WIRESHARK/#https://qiniu.xiaobeing.cn/#g' install_pnetlab_v6.sh
 bash download_pnetlab_v6.sh
 wget https://github.com/MoeClub/OneList/raw/master/OneDriveUploader/amd64/linux/OneDriveUploader
 chmod 777 OneDriveUploader
