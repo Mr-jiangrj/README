a4812c8eec2f3d7816f51e6e818a709f55dd3780
XiaoBeing <94702632+Mr-jiangrj@users.noreply.github.com>
2023-04-17 16:45:35 +0800

    Update sed.sh

--- ishare2/sed.sh
+++ ishare2/sed.sh
@@ -1,1 +1,4 @@
-
+sed -i "s#=https://raw.githubusercontent.com/#=https://ghproxy.com/https://raw.githubusercontent.com/#g" /usr/sbin/ishare2
+sed -i "s#BIN_URL=$URL_PREFIX$BIN_GID$URL_POSTFIX#BIN_URL=https://ghproxy.com/https://github.com/Mr-jiangrj/website_build/raw/main/ishare2/bin.csv#g" /usr/sbin/ishare2
+sed -i "s#QEMU_URL=$URL_PREFIX$BIN_GID$URL_POSTFIX#QEMU_URL=https://ghproxy.com/https://github.com/Mr-jiangrj/website_build/raw/main/ishare2/bin.csv#g" /usr/sbin/ishare2
+sed -i "s#DYNAMIPS_URL=$URL_PREFIX$BIN_GID$URL_POSTFIX#DYNAMIPS_URL=https://ghproxy.com/https://github.com/Mr-jiangrj/website_build/raw/main/ishare2/bin.csv#g" /usr/sbin/ishare2
