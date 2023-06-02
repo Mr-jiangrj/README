7bbe44b3a34325d7cc40c7be7b683321c551e922
XiaoBeing <94702632+Mr-jiangrj@users.noreply.github.com>
2023-06-03 02:59:46 +0800

    Delete upload_onedrive.sh

--- upload_onedrive.sh
+++
@@ -1,8 +0,0 @@
-wget https://github.com/MoeClub/OneList/raw/master/OneDriveUploader/amd64/linux/OneDriveUploader
-chmod 777 OneDriveUploader
-mv OneDriveUploader /usr/bin/
-wget https://github.com/Mr-jiangrj/website_build/raw/main/onedrive.zip
-unzip onedrive.zip
-rm -rf onedrive.zip
-cd /opt/
-OneDriveUploader -t 18 -s "./" -r "Share"
